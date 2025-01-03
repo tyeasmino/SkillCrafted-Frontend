150ee2f9eacba2d0727062bfab867766211c66aa
Tamima Yeasmin <tyeasmino@gmail.com>
2025-01-03 22:18:29 +0600

    10.18 PM  update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link for navigation
-import ProjectCard from '../components/projectCard';
+import ProjectCard from '../components/ProjectCard';
  
 
 const Projects = () => {
