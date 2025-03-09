13697d6d20e2b48cdf16fb5bc90073a881b16bfa
Tamima Yeasmin <tyeasmino@gmail.com>
2025-03-10 03:20:30 +0600

    proposal user details

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -586,7 +586,7 @@ const ProjectDetails = () => {
                 {/* Modal for SkillCrafter details */}
                 {isModalOpen && (
                   <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center z-50">
-                    <div className="bg-white p-5 rounded-lg w-11/12 md:w-8/12 relative">
+                    <div className="bg-white p-5 rounded-lg w-11/12 md:w-8/12 h-[600px] md:h-auto overflow-scroll relative">
                       <button onClick={() => setIsModalOpen(false)} className="absolute bg-black text-white px-5 py-2 rounded-md top-2 right-2 text-sm">
                         Close
                       </button>
